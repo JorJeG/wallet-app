c3a706780007a3f1a4e6d9a285b4114e8c9fdbe9
Dmitry Rusakov <jkikksa@gmail.com>
2017-11-05 03:46:45 +0300

    тестовая реализация голосового управления

--- source/client/utils/index.js
+++ source/client/utils/index.js
@@ -1,42 +1,60 @@
-// eslint-disable-next-line
-export function recognizeSpeech() {
+let streamer = null;
 
-	if (typeof webkitSpeechRecognition !== 'function') {
-		console.warn('Распознование речи не поддерживается браузером!');
+if (typeof window === 'object') {
+	streamer = new ya.speechkit.SpeechRecognition();
+}
+
+const onTransfer = () => {
+	console.log('Нужно перевести бабки');
+};
+
+const onPay = (arr) => {
+	const value = arr.find((it) => !Number.isNaN(Number(it)));
+	console.log(`Нужно оплатить мобильник на сумму ${value}`);
+};
+
+const unknown = () => {
+	console.log('непонятная комманда');
+};
+
+const commandsMap = {
+	переведи: onTransfer,
+	оплати: onPay
+};
+
+const textParser = (text) => {
+	if (text.length === 0) {
 		return;
 	}
+	const arr = text.trim().split(' ');
 
-	const recognition = new webkitSpeechRecognition();
-	recognition.interimResults = false;
-	recognition.continuous = false;
-	recognition.lang = 'ru-RU';
-	recognition.start();
+	const command = arr.find((it) => commandsMap[it]);
+	(commandsMap[command] || unknown)(arr);
+};
 
-	recognition.onresult = (e) => {
-		const {transcript, confidence} = e.results[0][0];
-
-		console.log(`Вот что я услышал: ${transcript}, я уверен в этом на ${Math.round(confidence) * 100}%`);
-
-		if (confidence > 0.5) {
-			switch (transcript.toLowerCase()) {
-				case 'переведи мне денег на телефон':
-					console.log('ОКЕЙ)');
-					break;
-				case 'привет':
-					console.log('ЗДАРОВА');
-					break;
-				default:
-					console.log('Я ещё не знаю таких слов(');
-			}
-		} else {
-			console.log('Извини, я не тебя не понял');
-		}
-
-		recognition.stop();
-	};
-
-	recognition.onerror = (e) => {
-		console.log(e);
-		recognition.stop();
-	};
+// eslint-disable-next-line
+export function recognizeSpeech() {
+	if (typeof window === 'object') {
+		// 8a02e365-8087-42b1-b0e1-4b339fb615fd
+		streamer.start({
+			apikey: '8a02e365-8087-42b1-b0e1-4b339fb615fd',
+			initCallback() {
+				console.log('Началась запись звука.');
+			},
+			dataCallback(text, done) {
+				if (done) {
+					textParser(text);
+					streamer.stop();
+				}
+			},
+			errorCallback(err) {
+				console.log(`Возникла ошибка: ${err}`);
+			},
+			stopCallback() {
+				console.log('Запись звука прекращена.');
+			},
+			particialResults: false,
+			utteranceSilence: 60
+		});
+	}
 }
