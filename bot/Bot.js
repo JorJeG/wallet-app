8c1905ce0d54173e3d19809e15a175437f66c3ef
root <mister.0025@yandex.ru>
2017-11-06 12:36:31 +0000

    Bot's messages more informative

--- bot/Bot.js
+++ bot/Bot.js
@@ -3,6 +3,7 @@ const UsersModel = require('./../source/models/users');
 const CardsModel = require('./../source/models/cards');
 const TransactionsModel = require('./../source/models/transactions');
 const axios = require('axios');
+const CardInfo = require('card-info');
 
 const usersModel = new UsersModel();
 const cardsModel = new CardsModel();
@@ -12,10 +13,27 @@ const token = '454454123:AAHhzXuJat3R-Vh5BrVAlZPANvdNXoij0oM';
 
 const Bot = new TelegramBot(token, {polling: true});
 
+const axiosInstance = axios.create({baseURL: 'http://localhost:3001'});
+
 let operator;
 let options = {};
 let phoneNumber;
 let sum;
+
+function showCards(userId, userCardsId, allCards) {
+	let message = '';
+	let count = 1;
+	allCards.forEach((card) => {
+		if (userCardsId.includes(card.id)) {
+			const cardinfo = new CardInfo(card.cardNumber);
+			message += `${count}  ${card.cardNumber} (${cardinfo.bankName})\n`;
+			options[String(count)] = [card.cardNumber, cardinfo.bankName];
+			count += 1;
+		}
+	});
+
+	Bot.sendMessage(userId, `Ваши карты:\n${message}`);
+}
 
 Bot.onText(/^\/balance/, async (msg, match) => {
 	const userId = msg.chat.id;
@@ -23,18 +41,8 @@ Bot.onText(/^\/balance/, async (msg, match) => {
 	const userCardsId = user.card_id;
 
 	const allCards = await cardsModel.getAll();
+	showCards(userId, userCardsId, allCards);
 
-	let message = '';
-	let count = 1;
-	allCards.forEach((card) => {
-		if (userCardsId.includes(card.id)) {
-			message += `${count}  ${card.cardNumber}\n`;
-			options[String(count)] = card.cardNumber;
-			count += 1;
-		}
-	});
-
-	Bot.sendMessage(userId, message);
 	operator = 'balance';
 });
 
@@ -46,18 +54,8 @@ Bot.onText(/^\/cardToMobile (.+)/, async (msg, match) => {
 	phoneNumber = user.phoneNumber;
 
 	const allCards = await cardsModel.getAll();
+	showCards(userId, userCardsId, allCards);
 
-	let message = '';
-	let count = 1;
-	allCards.forEach((card) => {
-		if (userCardsId.includes(card.id)) {
-			message += `${count}  ${card.cardNumber}\n`;
-			options[String(count)] = card.cardNumber;
-			count += 1;
-		}
-	});
-
-	Bot.sendMessage(userId, message);
 	operator = 'cardToMobile';
 
 });
@@ -66,15 +64,15 @@ Bot.on('message', async (msg) => {
 	const userId = msg.chat.id;
 	if (operator === 'balance') {
 		if (options[msg.text]) {
-			const card = await cardsModel.getBy({cardNumber: options[msg.text]});
-			Bot.sendMessage(userId, `${String(card.balance)} руб`);
+			const card = await cardsModel.getBy({cardNumber: options[msg.text][0]});
+			Bot.sendMessage(userId, `Запрос баланса карты ${card.cardNumber} (${options[msg.text][1]})
+			${String(card.balance)} руб`);
 		}
 	}
 	else if (operator === 'cardToMobile') {
 		if (options[msg.text]) {
-			const card = await cardsModel.getBy({cardNumber: options[msg.text]});
-			await transactionsModel.create({cardId: card.Id, type: 'paymentMobile', data: {phoneNumber}, time: new Date().toISOString(), sum}, {});
-			Bot.sendMessage(userId, `${phoneNumber}   ${sum}`);
+			const card = await cardsModel.getBy({cardNumber: options[msg.text][0]});
+			await axiosInstance.post(`/cards/${card.id}/pay`, {phoneNumber, sum, mail: '', mailing: ''});
 		}
 	}
 });
